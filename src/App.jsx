680ba87af26a5c71faf3903f688855a7e6222f7d
Johan Castillo <jcjohan2707@gmail.com>
2021-01-20 14:46:28 -0400

    Changes in the structure project

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,22 @@
-import React, {Component} from 'react';
 import './App.css';
-
-// Image
-import image1 from './images/1.webp';
-import image2 from './images/2.webp';
-import image3 from './images/3.webp';
-import image4 from './images/4.webp';
-
-import icon1 from './images/icon1.svg'
-import icon2 from './images/icon2.svg'
-import icon3 from './images/icon3.svg'
-import icon4 from './images/icon4.svg'
-import icon5 from './images/icon5.svg'
-import icon6 from './images/icon6.svg'
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 
 
-class App extends Component{
-  render(){
+
+import Liqueurs from './pages/liqueurs/Liqueurs';
+
+
+
+
+const App = () => {
     return (
-      <div class="galeria">
-          <h1>Galeria Efecto Overlay</h1>
-          <div class="linea"></div>
-          <div class="contenedor-imagenes">
-              <div class="imagen">
-                  <img src={image1} alt="" />
-              </div>
-              <div class="imagen">
-                  <img src={image2} alt="" />
-              </div>
-              <div class="imagen">
-                  <img src={image3} alt="" />
-              </div>
-              <div class="imagen">
-                  <img src={image4} alt="" />
-              </div>
+        <Router>
 
-
-              <div class="imagen">
-                  <img src={icon1} alt="" />
-              </div>
-
-              <div class="imagen">
-                  <img src={icon2} alt="" />
-              </div>
-
-              <div class="imagen">
-                  <img src={icon3} alt="" />
-              </div>
-
-              <div class="imagen">
-                  <img src={icon4} alt="" />
-              </div>
-
-              <div class="imagen">
-                  <img src={icon5} alt="" />
-              </div>
-
-
-              <div class="imagen">
-                  <img src={icon6} alt="" />
-              </div>
-              
-
-              {/** 
-                <div class="imagen">
-                  <img src={image1} alt="" />
-                  <div class="overlay">
-                      <h2>Suscribete</h2>
-                  </div>
-              </div>
-                **/}
-          </div>
-      </div>
-    );
-  }
+            <Switch>
+                <Route exact path="/" component={Liqueurs} />
+            </Switch>
+        </Router>
+    )
 }
 
 export default App;
---
+++ src/pages/liqueurs/Liqueurs.jsx
@@ -0,0 +1,20 @@
+// Image
+import image1 from '../../images/liqueurs/1.webp'
+import image2 from '../../images/liqueurs/2.webp'
+import image3 from '../../images/liqueurs/3.webp'
+import image4 from '../../images/liqueurs/4.webp'
+import image5 from '../../images/liqueurs/5.png'
+
+const Liqueurs = () => {
+    return (
+        <div class="gallery">
+            <img src={image1} alt="" />
+            <img src={image2} alt="" />
+            <img src={image3} alt="" />
+            <img src={image4} alt="" />
+            <img src={image5} alt="" />
+        </div>
+    )
+}
+
+export default Liqueurs
